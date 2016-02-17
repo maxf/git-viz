8fb67c8cad5993301ee505b5fbfd34136e40482d
Max Froumentin <max@froumentin.net>
2016-02-17 00:02:34 +0000

    Group commits by date

--- main.js
+++ main.js
@@ -20,14 +20,31 @@ function setUpTimeScaleAndAxis(minDate, maxDate, margin) {
   return {time: time, timeAxis: timeAxis};
 }
 
+function groupByDay(prev, current) {
+  if (!prev.length) {
+    return [{date:current.date, count:1}];
+  }
+  var lastPrev = prev[prev.length-1];
+  if (lastPrev.date.toDateString() === current.date.toDateString()) {
+    prev[prev.length-1].count++; 
+    return prev;
+  } else {
+    current.count = 1;
+    prev.push({date:current.date, count:1});
+  }
+  return prev;
+}
+
+function formatCommit(commit) {
+  return {
+    hash: commit.substring(0,7),
+    date: new Date(commit.substring(8,37)),
+    comment: commit.substring(39)
+  };
+}
+
 d3.text('repo/log.txt', function(error, text) {
-  var commits = text.split('\n').map(function(commit) {
-    return {
-      hash: commit.substring(0,7),
-      date: new Date(commit.substring(8,37)),
-      comment: commit.substring(38)
-    };
-  });
+  var commits = text.split('\n').map(formatCommit).reduce(groupByDay, []);
 
   var minDate = commits[commits.length-1].date;
   var maxDate = commits[0].date;  
