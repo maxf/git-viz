3f6a31f39be3bda7a46547edac56e420fccac4cb
Max Froumentin <max@froumentin.net>
2016-02-17 18:19:09 +0000

    update

--- main.js
+++ main.js
@@ -4,74 +4,90 @@
 var margin = {top: 40, right: 40, bottom: 40, left:40},
     width = 800, height = 400;
 
-function setUpTimeScaleAndAxis(minDate, maxDate, margin) {
-  var time = d3.time.scale()
+var timeScale = (minDate, maxDate, margin) =>
+  d3.time.scale()
     .domain([minDate, d3.time.day.offset(maxDate, 1)])
     .rangeRound([0, width - margin.left - margin.right]);
 
-  var timeAxis = d3.svg.axis()
-    .scale(time)
+var timeAxis = (timeScale) =>
+  d3.svg.axis()
+    .scale(timeScale)
     .orient('bottom')
-    .ticks(d3.time.months, 3)
-    .tickFormat(d3.time.format('%m/%y'))
+    .ticks(10)
+    .tickFormat(d3.time.format('%m/%Y'))
     .tickSize(10, 0)
     .tickPadding(8);
-    
-  return {time: time, timeAxis: timeAxis};
-}
 
-function groupByDay(prev, current) {
-  if (!prev.length) {
-    return [{date:current.date, count:1}];
+
+
+function groupByDay(accumulatedDays, commit) {
+  if (!accumulatedDays.length) {
+    return [{date:commit.date, count:1}];
   }
-  var lastPrev = prev[prev.length-1];
-  if (lastPrev.date.toDateString() === current.date.toDateString()) {
-    prev[prev.length-1].count++; 
-    return prev;
+  var lastAccumulatedDay = accumulatedDays[accumulatedDays.length-1];
+  if (lastAccumulatedDay.date.toDateString() === commit.date.toDateString()) {
+    accumulatedDays[accumulatedDays.length-1].count++;
+    return accumulatedDays;
   } else {
-    current.count = 1;
-    prev.push({date:current.date, count:1});
+    accumulatedDays.push({date:commit.date, count:1});
   }
-  return prev;
+  return accumulatedDays;
 }
 
-function formatCommit(commit) {
+var logLineRegexp = /^([0-9a-f]{7})\|([^|]+)\|([^|].+)$/;
+
+function parseCommitLine(text) {
+  var fields = text.match(logLineRegexp);
   return {
-    hash: commit.substring(0,7),
-    date: new Date(commit.substring(8,37)),
-    comment: commit.substring(39)
-  };
+    hash: fields[1],
+    date: new Date(fields[2]),
+    comment: fields[3]
+  }
 }
 
-d3.text('repo/log.txt', function(error, text) {
-  var commits = text.split('\n').map(formatCommit).reduce(groupByDay, []);
+function isValidLogLine(line) {
+  return logLineRegexp.test(line);
+}
 
-  var minDate = commits[commits.length-1].date;
-  var maxDate = commits[0].date;  
-  var timeStuff = setUpTimeScaleAndAxis(minDate, maxDate, margin);
-  var time = timeStuff.time, timeAxis = timeStuff.timeAxis;
-  var svg = d3.select('#canvas')
+d3.text('repo/log.txt', (error, text) => {
+  var commitsPerDay = text
+    .split('\n')
+    .filter(isValidLogLine)
+    .map(parseCommitLine)
+    .reduce(groupByDay, [])
+  ;
+
+  var minDate = commitsPerDay[commitsPerDay.length-1].date;
+  var maxDate = commitsPerDay[0].date;
+  var tScale = timeScale(minDate, maxDate, margin);
+  var canvas = d3.select('#canvas')
     .append('g')
     .attr('width', width)
     .attr('height', height)
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  canvas.append('rect')
+    .attr('id', 'frame')
+    .attr('width', width)
+    .attr('height', height)
   ;
 
-  svg
-    .selectAll('circle')
-    .data(commits)
+  canvas.selectAll('circle')
+    .data(commitsPerDay)
     .enter()
-    .append('circle')
-      .attr('cx', function(commit) { return time(commit.date) })
-      .attr('cy', 100)
-      .attr('r', 2)
+    .append('rect')
+      .attr('x', commit => tScale(commit.date))
+      .attr('y', commit => height/2 - 5 * commit.count)
+      .attr('height', commit => 5 * commit.count)
+      .attr('width', 5)
       .attr('fill', 'red')
-  ;
-  
-  svg.append('g')
+      .append('title')
+        .text(commit => `${commit.date.toDateString()}: ${commit.count} commits.`)
+      ;
+
+  canvas.append('g')
     .attr('class', 'axis')
-    .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
-    .call(timeAxis)
+    .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
+    .call(timeAxis(tScale))
   ;
 
 });
--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@ NAME=`echo $1 | sed -e 's|.*/\([^/]*\)\.git|\1|g'`
 echo $NAME
 mkdir -p repo
 cd repo
+
+echo "Cloning repo"
 git clone $1
 cd $NAME
 
+echo "Extracting commit log"
 git log --no-merges --pretty=format:'%h|%cd|%s' > ../log.txt
 
