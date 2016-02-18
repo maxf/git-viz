32ac1203a16b0556e90cba359cfad8e452e76dc7
Max Froumentin <max@froumentin.net>
2016-02-18 23:47:15 +0000

    Don't clone, just take an existing repo

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-NAME=`echo $1 | sed -e 's|.*/\([^/]*\)\.git|\1|g'`
-echo $NAME
-mkdir -p repo
-cd repo
-
-echo "Cloning repo"
-git clone $1
-cd $NAME
+HERE=$PWD
+cd $1
 
 echo "Extracting commit log"
-git log --no-merges --pretty=format:'%h|%cd|%s' > ../log.txt
+git log --no-merges --date=iso8601-strict --pretty=format:'%h|%cd|%s' > $HERE/log.txt
 
