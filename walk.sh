367bb2f5d9d7a900f4c104472de50513ca6a1999
Max Froumentin <max@froumentin.net>
2016-02-23 20:22:48 +0000

    Cater for when tere are no js files

--- walk.sh
+++ walk.sh
@@ -7,9 +7,15 @@ git pull > /dev/null 2>&1
 while [ $? -eq 0 ]; do
     SHA=`git rev-parse HEAD`
     DATE=`git show -s --format=%ad --date=iso8601-strict`
-    ERRORS=`find . -name \*.js | xargs jshint | sed '/^$/d' | wc -l`
-    LINES=`find . -name \*.js | xargs wc -l|grep -o "[0-9]\+ total"|cut -d ' ' -f 1`
-    echo "$DATE / $SHA / $ERRORS / $LINES"
+    JSFILES=`find . -type f -name \*.js`
+    if [ -z "$JSFILES" ]; then
+      ERRORS=0
+      LINES=0
+    else
+      ERRORS=`echo $JSFILES | xargs jshint | sed '/^$/d' | wc -l`
+      LINES=`echo $JSFILES | xargs wc -l|grep -o "[0-9]\+ total"|cut -d ' ' -f 1`
+    fi
+    echo "$DATE,$SHA,$LINES,$ERRORS"
     git reset --hard "HEAD~" > /dev/null 2>&1
-    # Note: for things like code quality measurement, skipping commits makes sense (HEAD~4) is is much faster
+    # Note: for things like code quality measurement, skipping commits makes sense (HEAD~4) and is much faster
 done
