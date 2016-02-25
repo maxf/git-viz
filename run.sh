1d614016c3d530ad0c125388ae09ba14e64e75e7
Max Froumentin <max@froumentin.net>
2016-02-25 23:36:49 +0000

    Write log of latest commit to stdout

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-HERE=$PWD
 cd $1
 
-echo "Extracting commit log"
-git log --no-merges --date=iso8601-strict --pretty=format:'%h|%cd|%s' > $HERE/log.txt
+git pull
+
+git log --no-merges --date=iso8601-strict --pretty=format:'%h|%cd|%s'
 
