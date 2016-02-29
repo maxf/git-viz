d153a46c92186a4624229fde78b2ef92b670552f
Max Froumentin <max@froumentin.net>
2016-02-29 12:21:08 +0000

    use ordinal scale - I

--- main.js
+++ main.js
@@ -1,41 +1,29 @@
 /* global d3, moment */
 (function() {
 
-var margin = {top: 40, right: 40, bottom: 40, left:40},
-    width = 800, height = 400;
+var margin = {top: 40, right: 40, bottom: 40, left:40};
+var width = 800, height = 400;
 
-var timeScale = (minDate, maxDate, margin) =>
+var timeScale = (minDate, maxDate) =>
   d3.time.scale()
     .domain([minDate, d3.time.day.offset(maxDate, 1)])
     .rangeRound([0, width - margin.left - margin.right]);
 
-var timeAxis = (timeScale) =>
-  d3.svg.axis()
-    .scale(timeScale)
-    .orient('bottom')
-    .ticks(10)
-    .tickFormat(d3.time.format('%m/%Y'))
-    .tickSize(10, 0)
-    .tickPadding(8);
-
-var nbCommitsScale = (maxCommits) =>
-  d3.scale.linear()
-    .domain([0, maxCommits])
-    .range([200, 0]);
-
-var nbCommitsAxis = d3.svg.axis()
-  .scale(nbCommitsScale);
 
 function groupByWeek(accumulated, commit) {
   if (!accumulated.length) {
-    return [{date:commit.date, count:1}];
+    return [{date: commit.date, count:1, weekNumber: commit.weekNumber}];
   }
   var lastAccumulatedDay = accumulated[accumulated.length-1];
-  if (lastAccumulatedDay.date.week() === commit.date.week()) {
+  if (lastAccumulatedDay.weekNumber === commit.weekNumber) {
     accumulated[accumulated.length-1].count++;
     return accumulated;
   } else {
-    accumulated.push({date:commit.date, count:1});
+    accumulated.push({
+      date: commit.date,
+      count:1,
+      weekNumber: commit.weekNumber
+    });
   }
   return accumulated;
 }
@@ -44,64 +32,88 @@ var logLineRegexp = /^([0-9a-f]{7})\|([^|]+)\|([^|].+)$/;
 
 function parseCommitLine(text) {
   var fields = text.match(logLineRegexp);
-  return {
-    hash: fields[1],
-    date: moment(fields[2]),
-    comment: fields[3]
-  }
+  return d3.time.format.iso.parse(fields[2]);
 }
 
 function isValidLogLine(line) {
   return logLineRegexp.test(line);
 }
 
+var canvas = d3.select('#canvas')
+  .append('g')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+canvas.append('rect')
+  .attr('id', 'frame')
+  .attr('width', width)
+  .attr('height', height);
+
+
+
+
+
+
 d3.text('repo/log.txt', (error, text) => {
-  var commitsPerWeek = text
+  if (error) throw error;
+
+  var values = text
     .split('\n')
     .filter(isValidLogLine)
-    .map(parseCommitLine)
-    .reduce(groupByWeek, [])
-  ;
+    .map(parseCommitLine);
 
-  var maxWeeklyCommits = commitsPerWeek
-    .reduce((max, week) => Math.max(max, week.count), 0);
+  var x = timeScale(values[values.length - 1], values[0]);
 
-  var minDate = commitsPerWeek[commitsPerWeek.length-1].date;
-  var maxDate = commitsPerWeek[0].date;
-  var tScale = timeScale(minDate, maxDate, margin);
-  var nScale = nbCommitsScale(maxWeeklyCommits);
-  var canvas = d3.select('#canvas')
-    .append('g')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-  canvas.append('rect')
-    .attr('id', 'frame')
-    .attr('width', width)
-    .attr('height', height)
-  ;
+  var y = d3.scale.linear()
+    .domain([0, 20])
+    .range([height, 0]);
 
-  canvas.selectAll('circle')
-    .data(commitsPerWeek)
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%Y-%m"));
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+  var data = d3.layout.histogram()
+    .bins(x.ticks(20))
+    (values);
+
+  var bar = canvas.selectAll('.bar')
+    .data(data)
     .enter()
-    .append('rect')
-      .attr('x', commit => tScale(commit.date))
-      .attr('y', commit => nScale(commit.count))
-      .attr('height', 5)
-      .attr('width', 5)
+    .append('g').attr('class', 'bar')
+      .attr('transform', d => `translate(${x(d.x)}, ${y(d.y)})`)
       .attr('fill', 'red')
-      .append('title')
-        .text(commit => `Week ${commit.date.week()}: ${commit.count} commits.`);
 
-  canvas.append('g')
-    .attr('class', 'axis')
-    .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
-    .call(timeAxis(tScale));
+  bar.append('rect')
+    .attr('x', 1)
+    .attr('width', 18) // should be calculated from data[0].dx
+    .attr('height', d => height - y(d.y));
 
-//  canvas.append('g')
-//    .attr('class', 'axis')
-//    .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
-//    .call(nbCommitsAxis(nScale));
+  canvas.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+  canvas.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text('commits');
 
 });
 
